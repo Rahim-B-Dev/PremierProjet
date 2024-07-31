30e92ade2fd1689f6eb144e719ca2cfec1d6aaff
Rahim <rahim.ali.badaoui@gmail.com>
2024-07-31 10:28:44 +0200

    itération 1.3 sauvegarde localStorage

--- assets/js/preferences.js
+++ assets/js/preferences.js
@@ -1,2 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Lecture des paramètres sauvegardés dans le localStorage
+    const savedTheme = localStorage.getItem("theme");
+    const savedDisplay = localStorage.getItem("display");
+
+    // Appliquer les valeurs sauvegardées au formulaire si elles existent
+    if (savedTheme) {
+        document.querySelector("#theme-select").value = savedTheme;
+    }
+
+    if (savedDisplay) {
+        document.querySelector(
+            `input[name="radio-display"][value="${savedDisplay}"]`
+        ).checked = true;
+    }
+
+    // Stockage des paramètres dans le local storage à l'enregistrement du formulaire
+    document.querySelector("#save-button").addEventListener("click", function () {
+        // Récupérer la valeur sélectionnée dans le select
+        const selectedTheme = document.querySelector("#theme-select").value;
+
+        // Récupérer la valeur du bouton radio sélectionné
+        const selectedDisplayOption = document.querySelector(
+            'input[name="radio-display"]:checked'
+        ).value;
+
+        // Stocker les valeurs en localStorage
+        localStorage.setItem("theme", selectedTheme);
+        localStorage.setItem("display", selectedDisplayOption);
+    });
 });
